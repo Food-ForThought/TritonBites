60e8fa73f113e27717b8d7241a2f5ba71c58a29f
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2021-11-06 17:59:21 -0700

    Updated restaurant_view xml and modified fragment RestaurantDetailFragment

--- app/src/main/java/com/example/tritonbites/fragments/RestaurantDetailFragment.java
+++ app/src/main/java/com/example/tritonbites/fragments/RestaurantDetailFragment.java
@@ -2,65 +2,47 @@ package com.example.tritonbites.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.tritonbites.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RestaurantDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RestaurantDetailFragment extends Fragment {
+    public static final String TAG = "Restaurant Detail Fragment";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Button btPhoto;
+    private Button btCheckIn;
+    private Button btFavorite;
+    private RatingBar ratingBar;
+    //private RecyclerView rvInfo;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public RestaurantDetailFragment () {}
 
-    public RestaurantDetailFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RestaurantDetailFragment newInstance(String param1, String param2) {
-        RestaurantDetailFragment fragment = new RestaurantDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        btPhoto = view.findViewById(R.id.btPhoto);
+        btCheckIn = view.findViewById(R.id.btCheckIn);
+        btFavorite = view.findViewById(R.id.btFavorite);
+        ratingBar = view.findViewById(R.id.ratingBar);
+        //rvInfo = view.findViewById(R.id.rvInfo);
     }
 }
