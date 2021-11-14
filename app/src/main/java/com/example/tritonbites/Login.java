72f31629343fc1de4040116a7d1d49c2704aaa49
Mrinal Sharma <78994779+mrinals108@users.noreply.github.com>
2021-11-13 17:36:34 -0800

    Update Login.java

--- app/src/main/java/com/example/tritonbites/Login.java
+++ app/src/main/java/com/example/tritonbites/Login.java
@@ -1,13 +1,21 @@
 package com.example.tritonbites;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
