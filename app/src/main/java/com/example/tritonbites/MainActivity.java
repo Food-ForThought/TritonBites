df4aa0d8b02bafe190d43d70dd9ae0da2d78222d
Yash Puneet <yash.puneet02@gmail.com>
2021-11-13 18:01:09 -0800

    Updated MainActivity

--- app/src/main/java/com/example/tritonbites/MainActivity.java
+++ app/src/main/java/com/example/tritonbites/MainActivity.java
@@ -9,47 +9,45 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.tritonbites.R;
+import com.example.tritonbites.fragments.FoodDetailFragment;
+import com.example.tritonbites.fragments.FoodReviewFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     public static final String TAG = "MainActivity";
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
-    private BottomNavigationView bottomNavigationView;
+    //private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+       /* bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment fragment = new Fragment();
+                Fragment fragment;
                 switch (menuItem.getItemId()) {
-                    // TODO: Set to proper fragment
                     case R.id.action_home:
                         //fragment = new PostsFragment();
                         break;
-                    // TODO: Set to proper fragment
                     case R.id.action_compose:
                         //fragment = new ComposeFragment();
                         break;
-                    // TODO: Set to proper fragment
                     case R.id.action_profile:
-                    // TODO: Set to proper fragment
                     default:
                         //fragment = new ProfileFragment();
                         break;
-                }
-                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
-        });
-        // TODO: Set default selection to login
+        });*/
         //bottomNavigationView.setSelectedItemId(R.id.action_home);
-    }
 
+        fragmentManager.beginTransaction().replace(R.id.flContainer, new FoodDetailFragment()).commit();
+    }
 }
